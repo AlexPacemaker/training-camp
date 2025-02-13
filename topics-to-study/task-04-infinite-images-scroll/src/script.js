34aed6826be09a2da3207383d8103edec8af6a66
Kosulin <asabretooth@gmail.com>
2025-02-13 20:56:46 +0300

    hide api key

--- topics-to-study/task-04-infinite-images-scroll/src/script.js
+++ topics-to-study/task-04-infinite-images-scroll/src/script.js
@@ -1,14 +1,14 @@
 "use strict";
 
+import { API_KEY } from "../api-key.js";
+
 let page = 1;
 
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
-const apiKey = "lznQfoEJSFW9hWciT6-wOPObH8H9j_nE1mmCuYWVayY";
-
 const loadImages = async () => {
-  const response = await fetch(`https://api.unsplash.com/photos?page=${page}&client_id=${apiKey}`);
+  const response = await fetch(`https://api.unsplash.com/photos?page=${page}&client_id=${API_KEY}`);
   const data = await response.json();
   data.forEach((image) => {
     const imgElement = document.createElement("div");
